1c6b85639e87e556278c8b6bed4a51990e8d3392
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-07-03 17:51:50 +0300

    feat: types for static flags

--- packages/schema/src/types/builder-env/index.ts
+++ packages/schema/src/types/builder-env/index.ts
@@ -3,10 +3,20 @@ import type { WebpackImportMeta } from './webpack'
 
 export type BundlerImportMeta = ViteImportMeta & WebpackImportMeta
 
+interface MetaVersions extends Record<string, string> {
+  nuxt: string
+}
+
 declare global {
   interface ImportMeta extends BundlerImportMeta {
     /** the `file:` url of the current file (similar to `__filename` but as file url) */
     url: string
+
+    dev: boolean
+    server: boolean
+    client: boolean
+    nuxt: boolean
+    versions?: MetaVersions
 
     readonly env: Record<string, string | boolean | undefined>
   }
